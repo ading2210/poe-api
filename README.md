791f7a57c38a4b902c01deaeab330854ce344c62
ading2210 <ading2019@gmail.com>
2023-03-10 03:18:40 -0800

    add readme, license

---
+++ README.md
@@ -0,0 +1,8 @@
+# poe-api
+This is a reverse engineered API wrapper for Quora's poe.com.
+
+Currently, this is in early development and very incomplete.
+
+This program is licensed under the GNU GPL v3.
+
+![GNU GPL Logo](https://www.gnu.org/graphics/gplv3-with-text-136x68.png)
--- poe.py
+++ poe.py
@@ -1,9 +1,10 @@
-import requests, re, json
+import requests, re, json, random
 
 class Poe:
   gql_post = "https://poe.com/api/gql_POST"
   gql_post_recieve = "https://poe.com/api/gql_POST"
   home_url = "https://poe.com"
+  settings_url = "https://poe.com/api/settings"
 
   formkey = ""
   next_data = {}
@@ -15,6 +16,7 @@ class Poe:
     self.global_headers["cookie"] = cookie
     self.session = requests.Session()
     self.get_next_data()
+    self.channel = self.get_channel_data()
 
   def get_next_data(self):
     r = self.session.get(self.home_url, headers=self.global_headers)
@@ -27,3 +29,13 @@ class Poe:
     self.viewer = self.next_data["props"]["pageProps"]["payload"]["viewer"]
     
     return self.next_data
+  
+  def get_channel_data(self):
+    r = self.session.get(self.settings_url, headers=self.global_headers)
+    return r.json()["tchannelData"]
+
+  def get_websocket_url(self, channel=None):
+    if channel is None:
+      channel = self.channel
+    query = f'?min_seq={channel["minSeq"]}&channel={channel["channel"]}&hash={channel["channelHash"]}'
+    return f'wss://tch{random.randint(1, 1e6)}.tch.{channel["baseHost"]}/up/{channel["boxName"]}/updates'+query
