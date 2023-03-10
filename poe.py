8c12d789cd8588839acfb0afad211b1e04a8987c
ading2210 <ading2019@gmail.com>
2023-03-10 03:41:35 -0800

    add websocket connection

--- poe.py
+++ poe.py
@@ -1,4 +1,5 @@
 import requests, re, json, random
+import websocket
 
 class Poe:
   gql_post = "https://poe.com/api/gql_POST"
@@ -9,17 +10,18 @@ class Poe:
   formkey = ""
   next_data = {}
   bots = {}
-  global_headers = {}
   
   def __init__(self, cookie):
-    self.cookie = cookie
-    self.global_headers["cookie"] = cookie
     self.session = requests.Session()
+    self.ws = websocket.WebSocket()
+
+    self.cookie = cookie
+    self.session.headers.update({"cookie": cookie})
     self.get_next_data()
     self.channel = self.get_channel_data()
 
   def get_next_data(self):
-    r = self.session.get(self.home_url, headers=self.global_headers)
+    r = self.session.get(self.home_url)
     json_regex = r'<script id="__NEXT_DATA__" type="application\/json">(.+?)</script>'
     json_text = re.search(json_regex, r.text).group(1)
     self.next_data = json.loads(json_text)
@@ -31,7 +33,7 @@ class Poe:
     return self.next_data
   
   def get_channel_data(self):
-    r = self.session.get(self.settings_url, headers=self.global_headers)
+    r = self.session.get(self.settings_url)
     return r.json()["tchannelData"]
 
   def get_websocket_url(self, channel=None):
@@ -39,3 +41,8 @@ class Poe:
       channel = self.channel
     query = f'?min_seq={channel["minSeq"]}&channel={channel["channel"]}&hash={channel["channelHash"]}'
     return f'wss://tch{random.randint(1, 1e6)}.tch.{channel["baseHost"]}/up/{channel["boxName"]}/updates'+query
+  
+  def connect(self, url=None):
+    if url is None:
+      url = self.get_websocket_url()
+    self.ws.connect(url)
