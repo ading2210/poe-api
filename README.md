dec62c7b1e4caa7fadced9387427f8290fe40223
ading2210 <ading2019@gmail.com>
2023-03-15 02:00:22 -0700

    add graphql queries

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Currently, this is in early development and very incomplete.
 
 This program is licensed under the GNU GPL v3.
 
+The GraphQL queries are taken from [muharamdani/poe](https://github.com/muharamdani/poe), which is licenced under the ISC License. 
+
 ![GNU GPL Logo](https://www.gnu.org/graphics/gplv3-with-text-136x68.png)
--- poe.py
+++ poe.py
@@ -1,9 +1,15 @@
-import requests, re, json, random
+import requests, re, json, random, logging
 import websocket
+from pathlib import Path
+
+parent_path = Path(__file__).resolve().parent
+queries_path = parent_path / "graphql"
+
+logging.basicConfig()
+logger = logging.getLogger()
 
 class Poe:
   gql_post = "https://poe.com/api/gql_POST"
-  gql_post_recieve = "https://poe.com/api/gql_POST"
   home_url = "https://poe.com"
   settings_url = "https://poe.com/api/settings"
 
@@ -13,36 +19,45 @@ class Poe:
   
   def __init__(self, cookie):
     self.session = requests.Session()
-    self.ws = websocket.WebSocket()
 
     self.cookie = cookie
-    self.session.headers.update({"cookie": cookie})
-    self.get_next_data()
+    self.headers = {
+      "Cookie": cookie,
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+      "Referrer": "https://poe.com/",
+      "Origin": "https://poe.com",
+    }
+
+    self.session.headers.update(self.headers)
+    self.next_data = self.get_next_data()
     self.channel = self.get_channel_data()
 
+    self.gql_headers = {
+      "poe-formkey": self.formkey,
+      "poe-tchannel": self.channel["channel"],
+    }
+    self.gql_headers = {**self.gql_headers, **self.headers}
+    
   def get_next_data(self):
+    logger.info("Downloading next_data...")
     r = self.session.get(self.home_url)
     json_regex = r'<script id="__NEXT_DATA__" type="application\/json">(.+?)</script>'
     json_text = re.search(json_regex, r.text).group(1)
-    self.next_data = json.loads(json_text)
+    next_data = json.loads(json_text)
 
-    self.formkey = self.next_data["props"]["formkey"]
-    self.bots = self.next_data["props"]["pageProps"]["payload"]["viewer"]["availableBots"]
-    self.viewer = self.next_data["props"]["pageProps"]["payload"]["viewer"]
+    self.formkey = next_data["props"]["formkey"]
+    self.bots = next_data["props"]["pageProps"]["payload"]["viewer"]["availableBots"]
+    self.viewer = next_data["props"]["pageProps"]["payload"]["viewer"]
     
-    return self.next_data
+    return next_data
   
   def get_channel_data(self):
+    logger.info("Downloading channel data...")
     r = self.session.get(self.settings_url)
     return r.json()["tchannelData"]
-
+  
   def get_websocket_url(self, channel=None):
     if channel is None:
       channel = self.channel
     query = f'?min_seq={channel["minSeq"]}&channel={channel["channel"]}&hash={channel["channelHash"]}'
     return f'wss://tch{random.randint(1, 1e6)}.tch.{channel["baseHost"]}/up/{channel["boxName"]}/updates'+query
-  
-  def connect(self, url=None):
-    if url is None:
-      url = self.get_websocket_url()
-    self.ws.connect(url)
