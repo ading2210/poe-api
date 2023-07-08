6609d781b8a1363dbcf0bb4e1044e8a2dfd28d26
Cohee <titan0599@gmail.com>
2023-07-08 17:10:35 +0300

    #63 Get suggested replies (#152)

--- examples/send_message.py
+++ examples/send_message.py
@@ -1,6 +1,7 @@
 import poe
 import logging
 import sys
+import time
 
 #send a message and stream the response
 
@@ -8,7 +9,14 @@ token = sys.argv[1]
 poe.logger.setLevel(logging.INFO)
 client = poe.Client(token)
 
+def callback(suggestion):
+    print("Suggested reply:", suggestion)
+
 message = "Summarize the GNU GPL v3"
-for chunk in client.send_message("capybara", message, with_chat_break=True):
+for chunk in client.send_message("capybara", message, with_chat_break=True, suggest_callback=callback):
   print(chunk["text_new"], end="", flush=True)
 print()
+
+# Suggested replies usually come in a few seconds after the message is fully received
+for i in range(5):
+   time.sleep(1)
