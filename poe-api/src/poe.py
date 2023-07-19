8f03eb01d09af169850de9122388b93109de7c73
canxin <1969730106@qq.com>
2023-07-19 15:38:40 +0800

    update

--- poe-api/src/poe.py
+++ poe-api/src/poe.py
@@ -139,10 +139,6 @@ class Client:
     self.suggestion_callbacks = {}
 
     self.headers = {**headers, **{
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.0.0',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*q=0.8,application/signed-exchange;v=b3;q=0.7',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
       'Cache-Control': 'max-age=0',
       'Sec-Ch-Ua': '"Microsoft Edge";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
       'Sec-Ch-Ua-Mobile': '?0',
@@ -150,7 +146,6 @@ class Client:
       'Sec-Fetch-Mode': 'navigate',
       'Sec-Fetch-Site': 'same-origin',
       'Sec-Fetch-User': '?1',
-      'Upgrade-Insecure-Requests': '1'
     }}
 
     self.connect_ws()
