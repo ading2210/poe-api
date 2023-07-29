be5929a9c5880ea97282ba36a186ea85d225cdf7
ading2210 <ading2019@gmail.com>
2023-07-29 18:23:45 -0400

    convert to package instead of module

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ long_description = (base_path / "README.md").read_text()
 
 setuptools.setup(
   name="poe-api",
-  version="0.4.17",
+  version="0.5.0",
   author="ading2210",
   license="GPLv3",
   description="A reverse engineered API wrapper for Quora's Poe",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  packages=["poe_graphql"],
+  packages=["poe"],
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: GNU General Public License (GPL)",
@@ -23,12 +23,11 @@ setuptools.setup(
   ],
   python_requires=">=3.7",
   package_dir={
-    "": "poe-api/src"
+    "": "src"
   },
   package_data={
-    "poe_graphql": ["*.graphql"]
+    "poe": ["*.graphql"]
   },
-  py_modules=["poe"],
   include_package_data=True,
   install_requires=[
     "websocket-client",
