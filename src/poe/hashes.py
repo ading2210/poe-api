0e09fcbb4c420713d7983840ddce3c154df97be9
ading2210 <ading2019@gmail.com>
2023-07-29 22:36:06 -0400

    extract queries from all js files

--- src/poe/hashes.py
+++ src/poe/hashes.py
@@ -1,6 +1,7 @@
 import tls_client as requests_tls
 import poe
 import re
+import json
 
 if __name__ == "__main__":
   session = requests_tls.Session(client_identifier="chrome112")
@@ -14,15 +15,28 @@ if __name__ == "__main__":
   session.headers.update(headers)
 
   r = session.get("https://poe.com/login?redirect_url=%2F")
-  scripts_regex = r'\"(https://psc2\.cf2\.poecdn\.net/[0-9a-f]{40}/_next/static/chunks.+?.js)"'
-  script_urls = re.findall(scripts_regex, r.text)
-  
-  for url in script_urls:
-    r2 = session.get(url)
+  manifest_regex = r'https://psc2\.cf2\.poecdn\.net/[0-9a-f]{40}/_next/static/\S{21}/_buildManifest\.js'
+  base_regex = r'https://psc2\.cf2\.poecdn\.net/[0-9a-f]{40}/_next/'
+  manifest_url = re.findall(manifest_regex, r.text)[0]
+  base_url = re.findall(base_regex, r.text)[0]
+
+  r2 = session.get(manifest_url)
+  resources_regex = r'"(static/.+?)"'
+  resources_list = re.findall(resources_regex, r2.text)
+
+  queries = {}
+  for resource in resources_list:
+    if not resource.endswith(".js"):
+      continue
+    url = base_url + resource
+
+    r3 = session.get(url)
     hashes_regex = r'params:{id:"([0-9a-f]{64})".+?name:"(\S+?)"'
-    hashes_list = re.findall(hashes_regex, r2.text)
+    hashes_list = re.findall(hashes_regex, r3.text)
 
     for query_hash, query_name in hashes_list:
       if "_" in query_name:
         query_name = query_name.split("_")[1]
-      print(query_hash, query_name)
+      queries[query_name] = query_hash
+
+  print(json.dumps(queries, indent=2, sort_keys=True))
